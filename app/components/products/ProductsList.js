7a740c734f9496e8469a3659dae16cc4deb62a80
Matias Sanguinet <rmsrcel@gmail.com>
2024-02-25 19:57:23 -0300

    prueba vercel url a fuego

--- app/components/products/ProductsList.js
+++ app/components/products/ProductsList.js
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 const getProdByCategory = async (categoria) => {
   const items = await fetch(
-    `https://${process.env.VERCEL_URL}/api/productos/${categoria}`,
+    `https://nextjs-msanguinet.vercel.app/api/productos/${categoria}`,
 
     {
       cache: 'no-store',
