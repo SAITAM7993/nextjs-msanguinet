818318877d4f2d1dee2d55bdc6d35de24e008f64
Matias Sanguinet <rmsrcel@gmail.com>
2024-02-25 20:05:25 -0300

    prueba vercel 5

--- app/components/products/ProductDetail.js
+++ app/components/products/ProductDetail.js
@@ -7,7 +7,8 @@ import GoBack from '../ui/GoBack';
 
 const getProductDetail = async (slug) => {
   const item = await fetch(
-    `http://${process.env.VERCEL_URL}/api/producto/${slug}`,
+    // `http://${process.env.VERCEL_URL}/api/producto/${slug}`,
+    `http://nextjs-msanguinet.vercel.app/api/producto/${slug}`,
     {
       cache: 'no-store',
       //para que no se cachee una la respuesta y siempre este actualizada por si varia el stock
